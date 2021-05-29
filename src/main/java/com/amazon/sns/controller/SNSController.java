9db06e7040f887faca0a9e9dc5458c38ce34e30e
Subrat Pani <subrat.pani@airtel.com>
2021-05-29 09:58:47 +0530

    added UI code

--- src/main/java/com/amazon/sns/controller/SNSController.java
+++ src/main/java/com/amazon/sns/controller/SNSController.java
@@ -1,6 +1,8 @@
 package com.amazon.sns.controller;
 
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,8 +11,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class SNSController {
 
     @RequestMapping(path = "/notification", method = {RequestMethod.GET, RequestMethod.POST})
-    public String index(@RequestBody(required = false) String snsSubscriptionRequest) {
+    public String index(@RequestBody(required = false) String snsSubscriptionRequest, Model model) {
         System.out.println("SNS Subscription Request  : " + snsSubscriptionRequest);
+        if (snsSubscriptionRequest != null) {
+            JSONObject jsonObject = new JSONObject(snsSubscriptionRequest);
+            String subscriptionURL = jsonObject.getString("SubscribeURL");
+            model.addAttribute("subscriptionURL", subscriptionURL);
+        }
         return "index";
     }
 
