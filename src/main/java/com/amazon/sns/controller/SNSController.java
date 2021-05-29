2bf5a4865f9d4d62adb68ad011f9a582da5732fc
Subrat Pani <subrat.pani@airtel.com>
2021-05-29 13:47:46 +0530

    refactor

--- src/main/java/com/amazon/sns/controller/SNSController.java
+++ src/main/java/com/amazon/sns/controller/SNSController.java
@@ -1,6 +1,5 @@
 package com.amazon.sns.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
@@ -9,20 +8,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @Controller
 public class SNSController {
 
     static ObjectMapper objectMapper = new ObjectMapper();
 
     @RequestMapping(path = "/notification", method = {RequestMethod.GET, RequestMethod.POST})
-    public ModelAndView index(@RequestBody(required = false) String snsSubscriptionRequestString) throws JsonProcessingException {
+    public ModelAndView index(@RequestBody(required = false) String snsSubscriptionRequestString, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException {
         System.out.println("SNS Subscription Request String : " + snsSubscriptionRequestString);
         ModelAndView modelAndView = new ModelAndView("index");
         if (snsSubscriptionRequestString != null) {
             snsSubscriptionRequestString = snsSubscriptionRequestString.replace("\n", "");
             JSONObject jsonObject = new JSONObject(snsSubscriptionRequestString);
             final String subscribeURL = jsonObject.optString("SubscribeURL");
-            modelAndView = new ModelAndView("notification");
+            httpServletResponse.sendRedirect(subscribeURL);
             modelAndView.addObject("subscribeURL", subscribeURL);
         }
         return modelAndView;
