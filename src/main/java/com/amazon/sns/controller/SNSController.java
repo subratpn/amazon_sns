1c2630643d6fdc284b7ddc2ca70d55127dc8f9f1
Subrat Pani <subrat.pani@airtel.com>
2021-05-29 10:53:16 +0530

    refactor

--- src/main/java/com/amazon/sns/controller/SNSController.java
+++ src/main/java/com/amazon/sns/controller/SNSController.java
@@ -4,23 +4,25 @@ import com.amazon.sns.pojo.SNSSubscriptionRequest;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SNSController {
 
     static ObjectMapper objectMapper = new ObjectMapper();
+
     @RequestMapping(path = "/notification", method = {RequestMethod.GET, RequestMethod.POST})
-    public String index(@RequestBody(required = false) String snsSubscriptionRequestString, Model model) throws JsonProcessingException {
+    public ModelAndView index(@RequestBody(required = false) String snsSubscriptionRequestString) throws JsonProcessingException {
         System.out.println("SNS Subscription Request String : " + snsSubscriptionRequestString);
+        ModelAndView modelAndView = new ModelAndView("index");
         if (snsSubscriptionRequestString != null) {
-            SNSSubscriptionRequest snsSubscriptionRequest =  objectMapper.readValue(snsSubscriptionRequestString, SNSSubscriptionRequest.class);
-            model.addAttribute("snsSubscriptionRequest", snsSubscriptionRequest);
+            SNSSubscriptionRequest snsSubscriptionRequest = objectMapper.readValue(snsSubscriptionRequestString, SNSSubscriptionRequest.class);
+            modelAndView.addObject("snsSubscriptionRequest", snsSubscriptionRequest);
         }
-        return "index";
+        return modelAndView;
     }
 
 
