471f7719846bcfe51c44a19876dd0ee982ca8e50
Subrat Pani <subrat.pani@airtel.com>
2021-05-30 10:43:17 +0530

    refactor

--- src/main/java/com/amazon/sns/controller/SNSController.java
+++ src/main/java/com/amazon/sns/controller/SNSController.java
@@ -1,7 +1,6 @@
 package com.amazon.sns.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Controller
 public class SNSController {
@@ -17,18 +15,9 @@ public class SNSController {
     static ObjectMapper objectMapper = new ObjectMapper();
 
     @RequestMapping(path = "/notification", method = {RequestMethod.GET, RequestMethod.POST})
-    public String index(@RequestBody(required = false) String snsSubscriptionRequestString, HttpServletRequest httpServletRequest,
-                        HttpServletResponse httpServletResponse) throws IOException {
-        System.out.println("SNS Subscription Request String : " + snsSubscriptionRequestString);
-        String view = "index";
-        if (snsSubscriptionRequestString != null) {
-            snsSubscriptionRequestString = snsSubscriptionRequestString.replace("\n", "");
-            JSONObject jsonObject = new JSONObject(snsSubscriptionRequestString);
-            final String subscribeURL = jsonObject.optString("SubscribeURL");
-            System.out.println(subscribeURL);
-            httpServletResponse.sendRedirect(subscribeURL);
-        }
-        return view;
+    public void index(@RequestBody(required = false) String snsRequest, HttpServletRequest httpServletRequest,
+                      HttpServletResponse httpServletResponse) {
+        System.out.println("SNS Request String : " + snsRequest);
     }
 
 }
